2b4262ae361088ca5cad66fb4af2c330ebfeebbe
Omkar <goa11@iitbbs.ac.in>
2023-12-01 09:00:02 +0530

    chore: models: use optional correctly

--- server/app/models/book.py
+++ server/app/models/book.py
@@ -1,19 +1,21 @@
 from datetime import datetime
-from typing import List
+from typing import List, Optional
+
 from pydantic import BaseModel, Field
 
 
 class Book(BaseModel):
-    id: int = Field(...,
-                        description="Primary key - integer id of the book")
+    id: int = Field(..., description="Primary key - integer id of the book")
     title: str = Field(..., description="Primary key - title of the book")
     authors: List[str] = Field(..., description="Author(s) of the book")
-    isbn: str = Field(None, description="ISBN of the book")
-    isbn13: str = Field(None, description="ISBN13 of the book")
-    language_code: str = Field(None, description="Language code of the book")
-    num_pages: int = Field(None, description="Number of pages in the book")
-    publication_date: datetime = Field(None,
-                                  description="Publication date of the book")
-    publisher: str = Field(None, description="Publisher of the book")
+    isbn: Optional[str] = Field(None, description="ISBN of the book")
+    isbn13: Optional[str] = Field(None, description="ISBN13 of the book")
+    language_code: Optional[str] = Field(None, description="Language code of the book")
+    num_pages: Optional[int] = Field(None, description="Number of pages in the book")
+    publication_date: Optional[datetime] = Field(
+        None, description="Publication date of the book"
+    )
+    publisher: Optional[str] = Field(None, description="Publisher of the book")
     created_at: datetime = Field(
-        ..., description="Datetime when the book was added to the platform")
+        ..., description="Datetime when the book was added to the platform"
+    )
--- server/app/models/user.py
+++ server/app/models/user.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from pydantic import BaseModel, Field
 
@@ -6,5 +7,7 @@ from pydantic import BaseModel, Field
 class User(BaseModel):
     id: int = Field(..., description="Primary key - integer id of the user")
     email: str = Field(..., description="Email id of the user")
-    name: str = Field(None, description="Name of the user")
-    created_at: datetime = Field(..., description="Datetime when the user signed up to the platform")
+    name: Optional[str] = Field(None, description="Name of the user")
+    created_at: datetime = Field(
+        ..., description="Datetime when the user signed up to the platform"
+    )
