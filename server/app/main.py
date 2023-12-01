179260c495384c3f093de0c27715715225a0a193
Omkar <goa11@iitbbs.ac.in>
2023-12-01 08:52:35 +0530

    chore: drop deprecated startup events
    * It is all lifespan now

--- server/app/main.py
+++ server/app/main.py
@@ -5,29 +5,29 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.database import DatabaseConnectionPool
 from app.routes import router
 
-app = FastAPI(title="lib-next")
+
+async def database_connection(app: FastAPI):
+    print("Initialising database connection pool")
+    load_status = load_dotenv(dotenv_path=".env")
+    print(f"Loaded .env file: {load_status}")
+    await DatabaseConnectionPool.create()
+    print("Initialized database connection pool")
+
+    yield
+
+    print("Closing database connection pool")
+    await DatabaseConnectionPool.close()
+
+
+app = FastAPI(title="lib-next", lifespan=database_connection)
 app.include_router(router, prefix="/api/v1")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"])
-
-
-@app.on_event('startup')
-async def init():
-    print("Performing init tasks")
-    loaded = load_dotenv(dotenv_path='.env')
-    print(f"Loaded .env file: {loaded}")
-    await DatabaseConnectionPool.create()
-    print("Initialized database connection pool")
-
-
-@app.on_event('shutdown')
-async def finalise():
-    await DatabaseConnectionPool.close()
-    print("Closed database connection pool")
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
