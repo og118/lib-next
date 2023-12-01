513207a82f4bac6d2e5ac702313dab2eda6ffd3c
Omkar <goa11@iitbbs.ac.in>
2023-12-01 09:12:20 +0530

    feat: add created_at field to users table
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- server/delete_and_recreate_tables.py
+++ server/delete_and_recreate_tables.py
@@ -15,7 +15,7 @@ async def drop_all_tables_if_present(connection):
     result = await connection.fetch(query)
 
     tables = [f"{schema_name}.{row['tablename']}" for row in result]
-    tables = ', '.join(tables)
+    tables = ", ".join(tables)
     query = f"DROP TABLE IF EXISTS {tables} CASCADE;"
     print(f"Executing query: {query}")
     await connection.execute(query)
@@ -25,7 +25,8 @@ async def create_user_table(connection):
     query = f"""CREATE TABLE {schema_name}.user (
         id              SERIAL PRIMARY KEY,
         name            VARCHAR(64) NULL,
-        email           VARCHAR(64) UNIQUE NOT NULL
+        email           VARCHAR(64) UNIQUE NOT NULL,
+        created_at      TIMESTAMP NOT NULL DEFAULT NOW()
     );"""
 
     print(f"Executing query: {query}")
@@ -42,7 +43,9 @@ async def create_book_table(connection):
         language_code           VARCHAR(10) NULL,
         num_pages               INT NULL,
         publication_date        DATE NULL,
-        publisher               VARCHAR(255) NULL
+        publisher               VARCHAR(255) NULL,
+        created_at              TIMESTAMP NOT NULL DEFAULT NOW(),
+        CONSTRAINT book_title_authors_key UNIQUE (title, authors)
     );"""
 
     print(f"Executing query: {query}")
@@ -50,37 +53,55 @@ async def create_book_table(connection):
 
 
 def verify_local_environment():
-    if host_name != 'localhost' and host_name != '127.0.0.1' and host_name != 'postgres':
-        answer: str = input(f"Environment using host: {host_name} seems to be non local. "
-                            f"Are you sure you want to proceed? Type - Yes to continue or No to abort")
-        if answer.lower() == 'yes':
-            print(f"You have chosen to proceed with environment using host: {host_name}")
+    if (
+        host_name != "localhost"
+        and host_name != "127.0.0.1"
+        and host_name != "postgres"
+    ):
+        answer: str = input(
+            f"Environment using host: {host_name} seems to be non local. "
+            f"Are you sure you want to proceed? Type - Yes to continue or No to abort"
+        )
+        if answer.lower() == "yes":
+            print(
+                f"You have chosen to proceed with environment using host: {host_name}"
+            )
             time.sleep(1)
         else:
-            print(f"You have chosen not to proceed with environment using host: {host_name}")
+            print(
+                f"You have chosen not to proceed with environment using host: {host_name}"
+            )
             sys.exit(0)
-    elif db_name != 'testdb':
-        answer: str = input(f"Database: {db_name} seems to be other than the testdb. "
-                            f"Are you sure you want to proceed? Type - Yes to continue or No to abort")
-        if answer.lower() == 'yes':
-            print(f"You have chosen to proceed with environment using database: {db_name}")
+    elif db_name != "testdb":
+        answer: str = input(
+            f"Database: {db_name} seems to be other than the testdb. "
+            f"Are you sure you want to proceed? Type - Yes to continue or No to abort"
+        )
+        if answer.lower() == "yes":
+            print(
+                f"You have chosen to proceed with environment using database: {db_name}"
+            )
             time.sleep(1)
         else:
-            print(f"You have chosen not to proceed with environment using database: {db_name}")
+            print(
+                f"You have chosen not to proceed with environment using database: {db_name}"
+            )
             sys.exit(0)
     else:
-        print(f"Environment using host: {host_name} and database: {db_name} seems to be local testing.")
+        print(
+            f"Environment using host: {host_name} and database: {db_name} seems to be local testing."
+        )
 
 
 async def create_connection():
     return await asyncpg.connect(
-        host=os.environ.get('DB_HOST'),
-        port=os.environ.get('DB_PORT'),
-        database=os.environ.get('DB_NAME'),
-        user=os.environ.get('DB_USER'),
-        password=os.environ.get('DB_PASSWORD'),
-        timeout=int(os.environ.get('CONNECTION_TIMEOUT', 10)),
-        command_timeout=int(os.environ.get('QUERY_TIMEOUT', 60)),
+        host=os.environ.get("DB_HOST"),
+        port=os.environ.get("DB_PORT"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        timeout=int(os.environ.get("CONNECTION_TIMEOUT", 10)),
+        command_timeout=int(os.environ.get("QUERY_TIMEOUT", 60)),
     )
 
 
@@ -99,12 +120,12 @@ async def rebuild_schema():
     print(f"Closed connection")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_dotenv()
 
-    host_name = os.environ.get('DB_HOST')
-    db_name = os.environ.get('DB_NAME')
-    schema_name = os.environ.get('DB_SCHEMA')
+    host_name = os.environ.get("DB_HOST")
+    db_name = os.environ.get("DB_NAME")
+    schema_name = os.environ.get("DB_SCHEMA")
 
     verify_local_environment()
 
