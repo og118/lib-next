15f12f3a8f40ea45bd33ef170cb4470044f33a7f
Omkar <goa11@iitbbs.ac.in>
2023-12-02 11:43:01 +0530

    feat: add a basic error page

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Books } from "./views/Book";
+import ErrorPage from "./views/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/books",
